5ed78c761a66c786a8a7a5cf4e75fe642115ffbd
kmee0507 <kcmee@uvm.edu>
2017-01-20 10:32:35 -0500

    BlockBreaker
    Initial Build for a block breaker game in C#

---
+++ Assets/Scripts/Brick.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Brick : MonoBehaviour {
+	
+	public int maxHits;
+	public Sprite[] hitSprites;
+	
+	private int timesHit;
+	private LevelManager levelManager;
+	// Use this for initialization
+	void Start () {
+		timesHit = 0;
+		levelManager = GameObject.FindObjectOfType<LevelManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	
+	}
+	void OnCollisionEnter2D(Collision2D collision){
+		timesHit++;
+		
+		
+		if (timesHit >= maxHits) {
+			Destroy (gameObject);
+		} else {
+			LoadSprites();
+		}
+	}
+	
+	void LoadSprites () {
+		int spriteIndex = timesHit - 1;
+		this.GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+	}
+	
+	// TODO Remove this method once we can actually win!
+	void SimulateWin () {
+		levelManager.LoadNextLevel();
+	}
+}
